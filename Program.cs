ce670dee9089feb13809840fbb990311acaaedd4
AnatolyKul <84135820+AnatolyKul@users.noreply.github.com>
2021-05-24 11:22:08 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+	
+		class Program
+		{
+		    static void Main(string[] args)
+			{
+			  Треугольник a = new Треугольник(2, 4, 3, 2, 2, 4, 0);
+			  Квадрат b = new Квадрат(3, 4, 3, 2, 3, 5, 6);
+			  Параллелограмм c = new Параллелограмм(5, 4, 5, 2, 2, 3, 2);
+			  Трапеция d = new Трапеция(2, 4, 6, 2, 3, 4, 2);
+			   a.square();
+			   a.Perimetr();
+			   Console.WriteLine("");
+			   b.square();
+			   b.Perimetr();
+			   Console.WriteLine("");
+			   c.square();
+			   c.Perimetr();
+			   Console.WriteLine("");
+			   d.square();
+			   d.Perimetr();
+			   double T = b.square();
+			   double K = a.square();
+			  Console.WriteLine("");
+			  if (T<K)
+			  {
+				Console.WriteLine("Квадрат меньше треугольника, последний не поместится");
+			  }
+			  else
+			  {
+				Console.WriteLine("Квадрат больше треугольника, фигура поместится в квадрат");
+			  }
+
+
+
+
+
+
+
+
+
+
+
+		} 
+		}
+}
