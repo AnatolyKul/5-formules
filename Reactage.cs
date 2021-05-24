56a8ab95c2a9b66d2944aa4c150763050e11cea1
AnatolyKul <84135820+AnatolyKul@users.noreply.github.com>
2021-05-24 12:20:06 +0500

    Add files via upload

---
+++ Reactage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+	class Reactage :Shape
+	{
+		public Reactage (Point p1, Point p2, Point p3, Point p4)
+		{
+			points = new Point[4];
+			points[0] = p1;
+			points[1] = p2;
+			points[2] = p3;
+			points[3] = p4;
+		}
+		
+	}
+}
---
+++ Точки.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace ConsoleApp13
+{
+	class Shape
+	{
+		protected  Point[] points;
+		protected  Point center; 
+
+		public Shape()
+		{
+			points = null;
+		}
+		public Shape(Point center, Point[] points)
+		{
+			this.center = center;
+			this.points = points;
+		}
+		public Shape(Point[] points)
+		{ }
+
+		public void Draw()
+		{
+			if (points == null) return; 
+			
+
+			
+
+		}
+	}
+}
